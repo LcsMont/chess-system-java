d9ea610d7fb437ef054cf388ff918b4fd1920180
Lucas Monteiro <lucasmmnm@gmail.com>
2023-02-20 13:58:50 -0300

    Placing pieces on the board

---
+++ src/chess/chessPieces/King.java
@@ -0,0 +1,17 @@
+package chess.chessPieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+    
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
---
+++ src/chess/chessPieces/Rook.java
@@ -0,0 +1,17 @@
+package chess.chessPieces;
+
+import boardgame.Board;
+import chess.Color;
+import chess.ChessPiece;
+
+public class Rook extends ChessPiece {
+    
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
